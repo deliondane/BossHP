299fc3ff39aa3a31ec4723d30d1f9e17491f7228
deliondane <hynnk0@gmail.com>
2024-10-07 18:10:40 +0900

    1007

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ for(const item of bottomLi){
   //메뉴에 마우스를 올려놨을때 실행
   item.addEventListener('mouseenter', () => {
     //화살표 함수 내에서의 this는 상위객체인 window가 선택됨
+    console.log
     item.lastElementChild.style.maxHeight = item.lastElementChild.scrollHeight+"px";
   })
   //메뉴에서 마우스가 떠났을때 실행
@@ -12,3 +13,13 @@ for(const item of bottomLi){
     item.lastElementChild.style.maxHeight = "0px";
   })
 }
+
+//product_dot
+const dotOnList = document.querySelectorAll('.product_dot > li');
+
+dotOnList.forEach(dotOnList) => dotOnList.addEventListener("click",()=>{
+  for(let i = 0; i < dotOnList.length; i++){
+    dotOnList[i].classList.remove("on");
+  }
+  dotOnList.classList.add("on");
+})
